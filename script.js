3931efadc97b6015b3606b2adf00d24101934166
unnath123 <64188873+unnath123@users.noreply.github.com>
2023-09-01 12:08:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+
+let promise1= new Promise((resolve,reject)={
+	resolve(images[1].url);
+})
+
+let promise2= new Promise((resolve,reject)={
+	resolve(images[2].url);
+})
+
+let promise1= new Promise((resolve,reject)={
+	resolve(images[2].url);
+})
+
+let promises=[promise1,promise2,promises];
+
+Promise.all(promises)
+.then((data)=>{
+let img= document.createElement("img");
+	img.src="data";
+	output.appendChild(img);
+	})
+.catch((error)=>{
+	console.log(error)
+})
+
+
